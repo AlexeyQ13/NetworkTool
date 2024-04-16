78b812aead1aa65e10d024161f50e72473e0508b
AlexeyQ13 <gorshckov2019@gmail.com>
2024-04-17 01:57:16 +0300

    Изменения SpeedTest

--- NetworkTool/Pages/SpeedTestPage.xaml.cs
+++ NetworkTool/Pages/SpeedTestPage.xaml.cs
@@ -15,7 +15,7 @@ namespace NetworkTool.Pages
             InitializeComponent();
         }
 
-        private async void StartSpeedTestButton_Click(object sender, RoutedEventArgs e)
+/*        private async void StartSpeedTestButton_Click(object sender, RoutedEventArgs e)
         {
             var speedtestClient = new SpeedTestClient();
             var result = await speedtestClient.TestSpeedAsync(SpeedUnit.MBps);
@@ -25,6 +25,19 @@ namespace NetworkTool.Pages
 
             DownloadSpeed.Text = $"{result.DownloadSpeed} Мбит/с";
             UploadSpeed.Text = $"{result.UploadSpeed} Мбит/с";
+        }*/
+
+        private async void RunButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            var stClient = new SpeedTestClient();
+            stClient.ProgressChanged += (o, info) =>
+            {
+                Dispatcher.Invoke(() => Progress.Value += 1);
+                
+            };
+            var result = await stClient.TestSpeedAsync(SpeedUnit.Mbps);
+            DownloadSpeedTextBlock.Text = $"{result.DownloadSpeed} {result.SpeedUnit}";
+            UploadSpeedTextBlock.Text = $"{result.UploadSpeed} {result.SpeedUnit}";
         }
     }
 }
